e9ebb414d7ee113718861c9fc3258b66bc716f72
mahanttechsolution <112825700+mahanttechsolution@users.noreply.github.com>
2024-02-25 23:39:31 -0500

    Update views.py

--- manak/main/views.py
+++ manak/main/views.py
@@ -844,7 +844,7 @@ def excel_creation(data,shape,filename):
         {
         
         "font":"Tw Cen MT Condensed",
-        "font_size":8,
+        "font_size":9,
         "align": "center",
         "valign": "vcenter",
         "fg_color": "white",
